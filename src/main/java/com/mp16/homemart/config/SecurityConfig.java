45ed0a0841c6f4cf0cc3d2d0776e59cc1d8f1f9b
Fikry Farenza <vickyfarenza@gmail.com>
2022-03-10 20:35:35 +0700

    Done UI Login register :tada: :sparkles:

--- src/main/java/com/mp16/homemart/config/SecurityConfig.java
+++ src/main/java/com/mp16/homemart/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/register**", "/js/**", "/css/**", "/img/**")
+                .antMatchers("/register**", "/js/**", "/css/**", "/img/**", "/static/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
